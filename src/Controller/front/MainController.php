894f98e67bb92ebfdb13ea634686bb8cdd82958a
jeremy.le-goff <jeremy.le-goff@oclock.school>
2024-02-19 15:11:21 +0100

    modification controller

--- src/Controller/front/MainController.php
+++ src/Controller/front/MainController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller\front;
 
-use App\Model\Movie;
-use App\Repository\MovieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Model\Movie;
 
 /**
  * On étends AbstractController => c'est comme ca dans tous les controller dans symfony, car AbstractController nous offre plein de méthodes dont on va avoir besoin dans nos controller
@@ -18,12 +17,12 @@ class MainController extends AbstractController
      * 
      * @Route("/", name="home")
      */
-    public function home(MovieRepository $movieRepository)
+    public function home()
     {
-        // 1ere etape : on recupere les Movie depuis la bdd a l'aide du MovieRepository
-        $movies = $movieRepository->findAllOrderByReleaseDateDql();
-        // On retourne la vue twig home.html.twig
-        return $this->render('front/main/home.html.twig', [
+        $movies = Movie::getMovies();
+        // retourne la vue twig home.html.twig
+        return $this->render('main/home.html.twig', [
+            //je passe $movies à ma view
             'movies' => $movies
         ]);
     }
